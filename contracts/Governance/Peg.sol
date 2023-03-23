bbefe4fb150a825bc8c29c93388d77ffc754ee52
Dev1 <dev1@pegged.fi>
2023-03-24 00:14:45 +0200

    support for gonvernance

--- contracts/Governance/Peg.sol
+++ contracts/Governance/Peg.sol
@@ -1,9 +1,34 @@
+// SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.10;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract Peg is ERC20 {
+contract Peg is ERC20, ERC20Permit("Pegged Finance"), ERC20Votes {
     constructor(uint256 initalSuply) ERC20("Pegged Finance", "PEG") {
         _mint(msg.sender, initalSuply);
     }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+
 }
